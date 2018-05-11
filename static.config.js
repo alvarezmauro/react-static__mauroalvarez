3a79c8cb1ecec0bc8cb50621fb52e7ea6663556f
Mauro Alvarez <contact@mauroalvarez.com>
2018-05-11 12:26:27 +1000

    :heavy_plus_sign: Added SASS support

--- static.config.js
+++ static.config.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   getSiteData: () => ({
@@ -36,5 +37,39 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  webpack: (config, { defaultLoaders, stage }) => {
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use:
+              stage === 'dev'
+                ? [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+                : ExtractTextPlugin.extract({
+                  use: [
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: false,
+                      },
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: { includePaths: ['src/'] },
+                    },
+                  ],
+                }),
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
   },
 }
